6fa2d28edc6c06fc95f3764fa81e5f4b3e773631
Shae Williams <shaewilliams@live.com>
2023-02-15 16:59:06 +0100

    R script af voor plaatjes

--- Modelleren R script.R
+++ Modelleren R script.R
@@ -2,11 +2,35 @@ library("readxl")
 Data=read.csv("Battery_train.csv")
 x=seq(1,2058)
 y=rep(0.88,2058)
-plot(x,y,type='l',lty=2, col='red',ylim=c(0.85,1.1))
+plot(x,y,type='l',lty=2, col='red',ylim=c(0.85,1.1),xlab='Cycle #', ylab='Capacity (Ah)')
 text(1800,0.85,'Threshold: 0.88',col="red")
 for (i in seq(2,80)){
   lines(x,Data[[i]])
   
 }
 
+TotalCycles=c()
+for(i in seq(2,80)){
+  for(j in seq(1,2059)){
+    if(is.na(Data[[i]][j])){
+      TotalCycles=append(TotalCycles,j-1)
+      break
+    }
+  }
+  
+}
+
+plot(Data[[2]],seq(2058,1),type='l', xlab='Capacity (Ah)',ylab="RUL (Cycles)",xlim=c(0.86999,1.1))
+for(i in seq(3,80)){
+  lines(Data[[i]],append(seq(TotalCycles[i-1],1),rep(0,2058-TotalCycles[i-1])))
+}
+
+Data2=read.csv("Battery_test.csv")
+x=seq(1,1696)
+y=rep(0.88,1696)
+plot(x,y,type='l',lty=2,col='red',ylim=c(0.85,1.1),xlab="t (Cycles)", ylab="Capacity (Ah)")
+text(1200,0.85,"Threshold: 0.88", col='red')
+for(i in seq(2,11)){
+  lines(x,Data2[[i]])
+}
 
