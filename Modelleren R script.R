3ac1dff32868e5a8ae8455021af1fbe34f1e3380
Shae Williams <shaewilliams@live.com>
2023-02-15 16:14:13 +0100

    Eerste plotje

--- Modelleren R script.R
+++ Modelleren R script.R
@@ -1,2 +1,12 @@
 library("readxl")
-Data=read.csv("./Modelleren2A/Battery_train.csv")
+Data=read.csv("Battery_train.csv")
+x=seq(1,2058)
+y=rep(0.88,2058)
+plot(x,y,type='l',lty=2, col='red',ylim=c(0.85,1.1))
+text(1800,0.85,'Threshold: 0.88',col="red")
+for (i in seq(2,80)){
+  lines(x,Data[[i]])
+  
+}
+
+
